a822520a769e8ecfd162df4b4b2379c28c065159
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-12 16:01:45 +0300

    Modified PersonTest class to include a test case for getting name

--- src/test/java/components/persons/PersonTest.java
+++ src/test/java/components/persons/PersonTest.java
@@ -13,5 +13,11 @@ public class PersonTest {
         assertEquals(true, person instanceof Person);
     }
 
+    @Test
+    public void getName_GettingNameofInstantiatedPerson_string(){
+        Person person = new Person("Henry","[email protected]");
+        assertEquals("Henry",person.getName());
+    }
+
 
 }
