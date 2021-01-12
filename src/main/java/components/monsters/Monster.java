2136b4db3cb42e78a17c5b89bc144567f071fad3
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-12 21:03:52 +0300

    Modified Monster class to add a constructor

--- src/main/java/components/monsters/Monster.java
+++ src/main/java/components/monsters/Monster.java
@@ -2,4 +2,11 @@ package components.monsters;
 
 public class Monster {
 
+    private String name;
+    private int personId;
+
+    public Monster(String name,int personId){
+        this.name     = name;
+        this.personId = personId;
+    }
 }
