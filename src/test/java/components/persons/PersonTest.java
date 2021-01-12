712e58ec97fa3b36652035a34314de5fac75ef2d
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-12 16:07:29 +0300

    Modified PersonTest class to include a hepler method setUpNewPerson

--- src/test/java/components/persons/PersonTest.java
+++ src/test/java/components/persons/PersonTest.java
@@ -9,21 +9,25 @@ public class PersonTest {
 
     @Test
     public void Person_instantiatesCorrectly_true(){
-        Person person = new Person("Henry","[email protected]");
+        Person person = setUpNewPerson();
         assertEquals(true, person instanceof Person);
     }
 
     @Test
     public void getName_gettingNameOfInstantiatedPerson_string(){
-        Person person = new Person("Henry","[email protected]");
+        Person person = setUpNewPerson();
         assertEquals("Henry",person.getName());
     }
 
     @Test
     public void getEmail_gettingEmailOfInstantiatedPerson_string(){
-        Person person = new Person("Henry","[email protected]");
+        Person person = setUpNewPerson();
         assertEquals("[email protected]",person.getEmail());
     }
 
+    // HELPERS
+    public Person setUpNewPerson(){
+        return new Person("Henry","[email protected]");
+    }
 
 }
