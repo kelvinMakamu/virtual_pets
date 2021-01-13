69525611359af5e2f986a4e41b4f25db683fc805
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-13 11:51:19 +0300

    Modfied Monster class to add contants

--- src/main/java/components/monsters/Monster.java
+++ src/main/java/components/monsters/Monster.java
@@ -11,6 +11,14 @@ public class Monster {
     private int id;
     private String name;
     private int personId;
+    private int foodLevel;
+    private int sleepLevel;
+    private int playLevel;
+    // CONSTANTS
+    public static final int MAX_FOOD_LEVEL  = 3;
+    public static final int MAX_SLEEP_LEVEL = 8;
+    public static final int MAX_PLAY_LEVEL  = 12;
+    public static final int MIN_ALL_LEVELS  = 0;
 
     public Monster(String name,int personId){
         this.name     = name;
