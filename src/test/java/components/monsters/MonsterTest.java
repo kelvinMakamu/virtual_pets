ba05b0342ec53c344f3e82986ee4794ea34d923a
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-14 10:04:44 +0300

    Modified MonsterTest class to include a test case for depleteLevels  method

--- src/test/java/components/monsters/MonsterTest.java
+++ src/test/java/components/monsters/MonsterTest.java
@@ -107,6 +107,15 @@ public class MonsterTest {
     }
 
     @Test
+    public void depleteLevels_reduceAllLevels(){
+        Monster testMonster = setUpNewMonster();
+        testMonster.depleteLevels();
+        assertEquals(testMonster.getFoodLevel(), (Monster.MAX_FOOD_LEVEL / 2) - 1);
+        assertEquals(testMonster.getSleepLevel(), (Monster.MAX_SLEEP_LEVEL / 2) - 1);
+        assertEquals(testMonster.getPlayLevel(), (Monster.MAX_PLAY_LEVEL / 2) - 1);
+    }
+
+    @Test
     public void play_increasesMonsterPlayLevel(){
         Monster monster = setUpNewMonster();
         monster.play();
